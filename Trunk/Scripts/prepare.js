09fe7c8ffb20d90b2b289e1ff648cf4c4b927804
unknown <Soev@SOEV.domain.corp>
2015-02-06 21:16:44 +0300

    divs ads are also to be hidden as well as iframes

--- Trunk/Scripts/prepare.js
+++ Trunk/Scripts/prepare.js
@@ -5,9 +5,25 @@ function prepare()
 	for (var i = 0; i < iframes.length; i++)
 	{
 		var iframe = iframes[i];
-		if (iframe.name != 'main' && iframe.name != 'news')
+		if (iframe.name != 'main' && iframe.name != 'news' && iframe.style.display !='none')
+		{
+			//alert('Hiding iframe: ' + iframe.id);
 			iframe.style.display='none';
+		}
 	}
+	
+	var divs = document.getElementsByTagName('div');
+	for (var i = 0; i < divs.length; i++)
+	{
+		var div = divs[i];
+		if (div.id != 'main' && div.id != 'news' && div.id.length != 0 && div.style.display !='none')
+		{
+			//alert('Hiding div: ' + div.id);
+			div.style.display='none';
+		}
+	}
+	
+	window.setTimeout(prepare, 5000);
 }
 
 window.onload = prepare;
