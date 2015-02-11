aa8ea423e31d0618ea5a8dbc9774257aab6f8869
unknown <Soev@SOEV.domain.corp>
2015-02-11 21:21:08 +0300

    May99 pages changed to be shown from ucoz hosting without ads

--- Trunk/Scripts/prepare.js
+++ Trunk/Scripts/prepare.js
@@ -5,7 +5,9 @@ function prepare()
 	for (var i = 0; i < iframes.length; i++)
 	{
 		var iframe = iframes[i];
-		if (iframe.name != 'main' && iframe.name != 'news' && iframe.style.display !='none')
+		if (iframe.name != 'main' && iframe.name != 'news'  && // main page
+			iframe.name != 'title' && iframe.name != 'members' && iframe.name != 'stories' && // may99 page
+			iframe.style.display !='none')
 		{
 			//alert('Hiding iframe: ' + iframe.id);
 			iframe.style.display='none';
@@ -16,7 +18,9 @@ function prepare()
 	for (var i = 0; i < divs.length; i++)
 	{
 		var div = divs[i];
-		if (div.id != 'main' && div.id != 'news' && div.id.length != 0 && div.style.display !='none')
+		if (div.id != 'main' && div.id != 'news' && // home page
+			div.id != 'title' && div.id != 'frames' && div.id != 'row' && div.id != 'members' && div.id != 'stories' && // may99 page
+			div.id.length != 0 && div.style.display !='none')
 		{
 			//alert('Hiding div: ' + div.id);
 			div.style.display='none';
